e1c69d713513f2d253c3e17359497b94f3d35f95
Alina <alinagusarik@gmail.com>
2021-09-02 20:30:07 +0300

    feat: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,33 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    a = 0
+    constructor() {
+        
+        
     }
-
+ 
+    setRange(min, max) {
+        this.min = min
+        this.max = max
+    }
     guess() {
-
+        
+       return this.a = Math.round((this.max - this.min) / 2 + this.min)
+        
     }
 
     lower() {
+         
+       return this.max = this.a 
+        
+     this.guess()
 
     }
 
     greater() {
+        
+        return this.min = this.a
+        
+         this.guess()
 
     }
 }
